64a18aefa97bb080edb3c9f7761d11c58b1e9c60
Jeremy E Kozdon <jekozdon@nps.edu>
2021-06-14 14:57:54 -0700

    Remove circular testing

--- test/lib.jl
+++ test/lib.jl
@@ -5,16 +5,24 @@ using PETScX
     # This should error, not library Float64, Float64 is possible!
     @test_throws ErrorException PETScX.getpetsclib(Float64, Float64)
 
-    PETScX.@for_libpetsc begin
+    for petsclib in PETScX.petsclibs
+        # Get the library type
+        PetscLib = typeof(petsclib)
+
+        # Pull out the parameters
+        PetscScalar = PetscLib.parameters[1]
+        PetscInt = PetscLib.parameters[2]
+        PetscReal = real(PetscScalar)
+
         # Make sure we can find the library correctly
-        @test $petsclib === PETScX.getpetsclib($PetscScalar, $PetscInt)
+        @test petsclib === PETScX.getpetsclib(PetscScalar, PetscInt)
 
         # Check the type accessors
-        @test $PetscScalar === PETScX.scalartype($petsclib)
-        @test $PetscScalar === PETScX.scalartype($PetscLib)
-        @test $PetscInt === PETScX.inttype($petsclib)
-        @test $PetscInt === PETScX.inttype($PetscLib)
-        @test $PetscReal === PETScX.realtype($petsclib)
-        @test $PetscReal === PETScX.realtype($PetscLib)
+        @test PetscScalar === PETScX.scalartype(petsclib)
+        @test PetscScalar === PETScX.scalartype(PetscLib)
+        @test PetscInt === PETScX.inttype(petsclib)
+        @test PetscInt === PETScX.inttype(PetscLib)
+        @test PetscReal === PETScX.realtype(petsclib)
+        @test PetscReal === PETScX.realtype(PetscLib)
     end
 end
